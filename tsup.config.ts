d9d781e454f0f68fd67729ec918983093b3461df
selmansezer <selmansezer@sabanciuniv.edu>
2024-09-06 18:04:11 +0300

    Format code with Prettier

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,13 +1,13 @@
-import { defineConfig } from "tsup";
+import { defineConfig } from 'tsup';
 
 export default defineConfig({
-  entry: ["./src/index.ts"],
-  format: ["cjs", "esm", "iife"],
-  outDir: "lib",
+  entry: ['./src/index.ts'],
+  format: ['cjs', 'esm', 'iife'],
+  outDir: 'dist',
   dts: true,
   splitting: false,
   sourcemap: true,
   clean: true,
-  target: "esnext",
+  target: 'esnext',
   minify: true,
 });
