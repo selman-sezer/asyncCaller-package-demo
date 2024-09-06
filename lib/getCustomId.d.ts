74464058b4b6b558934159f40849d1bbcedf6035
selmansezer <selmansezer@sabanciuniv.edu>
2024-09-06 12:10:00 +0300

    minor changes

--- lib/getCustomId.d.ts
+++
@@ -1,1 +0,0 @@
-export declare function getCustomIdText(id: string): string;
--- lib/tokenBucket/types.d.ts
+++
@@ -1,6 +0,0 @@
-export interface TokenBucketOptions {
-  capacity: number;
-  fillPerWindow: number;
-  windowInMs: number;
-  initialTokens?: number;
-}
