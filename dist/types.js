cbc125adcb0cbf0d9abd7356d1c27c37aeb5b6f0
selmansezer <selmansezer@sabanciuniv.edu>
2024-09-06 12:01:27 +0300

    deleted dist folder

--- dist/types.js
+++
@@ -1,9 +0,0 @@
-export var TimeUnit;
-(function (TimeUnit) {
-    TimeUnit[TimeUnit["Milliseconds"] = 1] = "Milliseconds";
-    TimeUnit[TimeUnit["Seconds"] = 1000] = "Seconds";
-    TimeUnit[TimeUnit["Minutes"] = 60000] = "Minutes";
-    TimeUnit[TimeUnit["Hours"] = 3600000] = "Hours";
-    TimeUnit[TimeUnit["Days"] = 86400000] = "Days";
-})(TimeUnit || (TimeUnit = {}));
-//# sourceMappingURL=types.js.map
